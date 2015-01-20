c220a6c3e44114e22d4d582cc29843ed5606c468
q858333 <284825870@qq.com>
2015-01-20 14:55:48 +0800

    视频播放添加 暂停，重播功能。

--- CustomVideoCapture/DBPlayVideoVC.m
+++ CustomVideoCapture/DBPlayVideoVC.m
@@ -14,6 +14,7 @@
    // MPMoviePlayerController *_player;
     
     UIProgressView   *_progressView;
+    UIButton         *_startBtn;
     
     BOOL  _playBeginState;
 }
@@ -28,9 +29,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor=[UIColor whiteColor];
-    //存图片
-    
 
+  
 
 
     AVURLAsset *movieAsset    = [[AVURLAsset alloc]initWithURL:self.fileURL options:nil];
@@ -53,17 +53,24 @@
     [self.player setAllowsExternalPlayback:YES];
     
     
-    UIButton *startBtn = [UIButton buttonWithType:UIButtonTypeSystem];
-    [startBtn setTitle:@"开始" forState:UIControlStateNormal];
-    startBtn.backgroundColor=[UIColor colorWithWhite:0.5 alpha:0.5];
-    startBtn.frame=self.view.layer.bounds;
-    [startBtn addTarget:self action:@selector(startBtnClick:) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:startBtn];
+    _startBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    [_startBtn setTitle:@"开始" forState:UIControlStateNormal];
+    _startBtn.backgroundColor=[UIColor colorWithWhite:0.5 alpha:0.5];
+    _startBtn.frame=self.view.layer.bounds;
+    [_startBtn addTarget:self action:@selector(startBtnClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:_startBtn];
     
     
     _progressView = [[UIProgressView alloc] initWithFrame:CGRectMake(0, 20, 0, 20)];
     _progressView.progress=1;
     [self.view addSubview:_progressView];
+    
+    
+    //播放结束
+    [[NSNotificationCenter defaultCenter] addObserver: self
+                                             selector: @selector(playerItemDidReachEnd:)
+                                                 name: AVPlayerItemDidPlayToEndTimeNotification
+                                               object: self.playerItem];
     
 
 //   _player  = [[MPMoviePlayerController alloc] initWithContentURL:self.fileURL];
@@ -79,11 +86,46 @@
     
     // Do any additional setup after loading the view.
 }
+
+
+
 -(void)startBtnClick:(UIButton *)sender
 {
-    [sender setTitle:@"" forState:UIControlStateNormal];
-    sender.backgroundColor=[UIColor clearColor];
-    [self.player play];
+    
+    NSLog(@"%d", self.player.actionAtItemEnd);
+    if([sender.titleLabel.text isEqualToString:@"开始"])
+    {
+        sender.titleLabel.text=@"";
+        [sender setTitle:@"" forState:UIControlStateNormal];
+
+        sender.backgroundColor=[UIColor clearColor];
+        [self.player play];
+
+
+    }else if ([sender.titleLabel.text isEqualToString:@"重播"])
+    {
+        
+        
+        sender.titleLabel.text=@"";
+        [sender setTitle:@"" forState:UIControlStateNormal];
+        
+        sender.backgroundColor=[UIColor clearColor];
+        
+        AVPlayerItem *playerItem = [self.player currentItem];
+        // Set it back to the beginning
+        [playerItem seekToTime: kCMTimeZero];
+        // Tell the player to do nothing when it reaches the end of the video
+        // -- It will come back to this method when it's done
+        self.player.actionAtItemEnd = AVPlayerActionAtItemEndNone;
+        // Play it again, Sam
+        [self.player play];
+
+    }else
+    {
+        [sender setTitle:@"开始" forState:UIControlStateNormal];
+        sender.backgroundColor=[UIColor colorWithWhite:0.5 alpha:0.5];
+        [self.player pause];
+    }
 
 }
 - (NSTimeInterval) playableDuration
@@ -149,6 +191,26 @@
     }
 }
 
+- (void)playerItemDidReachEnd: (NSNotification *)notification
+{
+    // Loop the video
+   NSLog(@"%@",notification.object);
+    [_startBtn setTitle:@"重播" forState:UIControlStateNormal];
+    _startBtn.backgroundColor=[UIColor colorWithWhite:0.5 alpha:0.5];
+
+        // Get the current item
+//        AVPlayerItem *playerItem = [self.player currentItem];
+//        // Set it back to the beginning
+//        [playerItem seekToTime: kCMTimeZero];
+//        // Tell the player to do nothing when it reaches the end of the video
+//        // -- It will come back to this method when it's done
+//        self.player.actionAtItemEnd = AVPlayerActionAtItemEndNone;
+//        // Play it again, Sam
+//        [self.player play];
+//    }else {
+//        mPlayer.actionAtItemEnd = AVPlayerActionAtItemEndAdvance;
+//    }
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
