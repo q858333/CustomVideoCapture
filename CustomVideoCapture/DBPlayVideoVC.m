1852802207e8bba8c0fc9c4d79665e00898c0d1f
q858333 <284825870@qq.com>
2015-01-20 16:01:53 +0800

    播放器添加暂停,重播功能

--- CustomVideoCapture/DBPlayVideoVC.m
+++ CustomVideoCapture/DBPlayVideoVC.m
@@ -9,14 +9,20 @@
 #import "DBPlayVideoVC.h"
 #import <MediaPlayer/MediaPlayer.h>
 #import <AVFoundation/AVFoundation.h>
+
+#define COUNT_DUR_TIMER_INTERVAL  0.025
+
 @interface DBPlayVideoVC ()
 {
    // MPMoviePlayerController *_player;
     
     UIProgressView   *_progressView;
     UIButton         *_startBtn;
+    NSTimer          *_timer;
+    NSInteger         _int_count;//播放时间
     
     BOOL  _playBeginState;
+    BOOL  _isPause;
 }
 
 @property (nonatomic ,strong) AVPlayer *player;
@@ -25,47 +31,18 @@
 @end
 
 @implementation DBPlayVideoVC
-
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor=[UIColor whiteColor];
 
-  
+    
+    [self initPlayer];//创建播放器
+    [self initView];//创建按钮 进度
 
-
-    AVURLAsset *movieAsset    = [[AVURLAsset alloc]initWithURL:self.fileURL options:nil];
-    
-    
-    self.playerItem = [AVPlayerItem playerItemWithAsset:movieAsset];
-    
-    [self.playerItem addObserver:self forKeyPath:@"status" options:0 context:NULL];
-    
-    self.player = [AVPlayer playerWithPlayerItem:self.playerItem];
-    
-    AVPlayerLayer *playerLayer = [AVPlayerLayer playerLayerWithPlayer:self.player];
-    
-    playerLayer.frame = self.view.layer.bounds;
-    
-    playerLayer.videoGravity = AVLayerVideoGravityResizeAspect;
-    
-    [self.view.layer addSublayer:playerLayer];
-    
-    [self.player setAllowsExternalPlayback:YES];
-    
-    
-    _startBtn = [UIButton buttonWithType:UIButtonTypeSystem];
-    [_startBtn setTitle:@"开始" forState:UIControlStateNormal];
-    _startBtn.backgroundColor=[UIColor colorWithWhite:0.5 alpha:0.5];
-    _startBtn.frame=self.view.layer.bounds;
-    [_startBtn addTarget:self action:@selector(startBtnClick:) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:_startBtn];
-    
-    
-    _progressView = [[UIProgressView alloc] initWithFrame:CGRectMake(0, 20, 0, 20)];
-    _progressView.progress=1;
-    [self.view addSubview:_progressView];
-    
-    
     //播放结束
     [[NSNotificationCenter defaultCenter] addObserver: self
                                              selector: @selector(playerItemDidReachEnd:)
@@ -87,47 +64,159 @@
     // Do any additional setup after loading the view.
 }
 
+-(void)initPlayer
+{
+    
+    
+    AVURLAsset *movieAsset    = [[AVURLAsset alloc]initWithURL:self.fileURL options:nil];
+    
+    
+    self.playerItem = [AVPlayerItem playerItemWithAsset:movieAsset];
+    
+    [self.playerItem addObserver:self forKeyPath:@"status" options:0 context:NULL];
+    
+    self.player = [AVPlayer playerWithPlayerItem:self.playerItem];
+    
+    AVPlayerLayer *playerLayer = [AVPlayerLayer playerLayerWithPlayer:self.player];
+    
+    playerLayer.frame = self.view.layer.bounds;
+    
+    playerLayer.videoGravity = AVLayerVideoGravityResizeAspect;
+    
+    [self.view.layer addSublayer:playerLayer];
+    
+    [self.player setAllowsExternalPlayback:YES];
+    
+    
 
+}
+-(void)initView
+{
+    _startBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    [_startBtn setTitle:@"开始" forState:UIControlStateNormal];
+    _startBtn.backgroundColor=[UIColor colorWithWhite:0.5 alpha:0.5];
+    _startBtn.frame=self.view.layer.bounds;
+    [_startBtn addTarget:self action:@selector(startBtnClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:_startBtn];
+    
+    
+    
+    _progressView = [[UIProgressView alloc] initWithFrame:CGRectMake(0, 20, 320, 20)];
+    _progressView.progress=0;
+    [self.view addSubview:_progressView];
+    
 
+}
 -(void)startBtnClick:(UIButton *)sender
 {
     
-    NSLog(@"%d", self.player.actionAtItemEnd);
     if([sender.titleLabel.text isEqualToString:@"开始"])
     {
-        sender.titleLabel.text=@"";
-        [sender setTitle:@"" forState:UIControlStateNormal];
 
-        sender.backgroundColor=[UIColor clearColor];
-        [self.player play];
-
+        [self startPlay];//开始
+       
 
     }else if ([sender.titleLabel.text isEqualToString:@"重播"])
     {
         
+        [self rePlay];//重播
         
-        sender.titleLabel.text=@"";
-        [sender setTitle:@"" forState:UIControlStateNormal];
-        
-        sender.backgroundColor=[UIColor clearColor];
-        
-        AVPlayerItem *playerItem = [self.player currentItem];
-        // Set it back to the beginning
-        [playerItem seekToTime: kCMTimeZero];
-        // Tell the player to do nothing when it reaches the end of the video
-        // -- It will come back to this method when it's done
-        self.player.actionAtItemEnd = AVPlayerActionAtItemEndNone;
-        // Play it again, Sam
-        [self.player play];
+    }else if ([sender.titleLabel.text isEqualToString:@"继续播放"])
+    {
+        [self continuePlay];//继续播放
 
     }else
     {
-        [sender setTitle:@"开始" forState:UIControlStateNormal];
-        sender.backgroundColor=[UIColor colorWithWhite:0.5 alpha:0.5];
-        [self.player pause];
+        [self pauesPlay];//暂停
     }
 
 }
+
+#pragma mark - 计时器操作
+-(void)startTimer
+{
+    _timer = [NSTimer scheduledTimerWithTimeInterval:COUNT_DUR_TIMER_INTERVAL target:self selector:@selector(onTimer) userInfo:nil repeats:YES];
+}
+
+-(void)onTimer
+{
+    
+    if (!_isPause)
+    {
+        _progressView.progress+=(COUNT_DUR_TIMER_INTERVAL/[self playableDuration]);
+    }
+
+}
+-(void)endTimer
+{
+    [_timer invalidate];
+    _timer=nil;
+}
+
+#pragma mark - 播放器状态
+//重播
+-(void)rePlay
+{
+    _startBtn.titleLabel.text=@"";
+    [_startBtn setTitle:@"" forState:UIControlStateNormal];
+    _startBtn.backgroundColor=[UIColor clearColor];
+
+   
+    AVPlayerItem *playerItem = [self.player currentItem];
+    // Set it back to the beginning
+    [playerItem seekToTime: kCMTimeZero];
+    // Tell the player to do nothing when it reaches the end of the video
+    // -- It will come back to this method when it's done
+    self.player.actionAtItemEnd = AVPlayerActionAtItemEndNone;
+    // Play it again, Sam
+    [self.player play];
+    
+    _progressView.progress=0;
+    _isPause=NO;
+    [self startTimer];
+
+}
+//继续播放
+-(void)continuePlay
+{
+    _startBtn.titleLabel.text=@"";
+    [_startBtn setTitle:@"" forState:UIControlStateNormal];
+    _startBtn.backgroundColor=[UIColor clearColor];
+    [self.player play];
+    _isPause=NO;
+
+
+}
+//暂停播放
+-(void)pauesPlay
+{
+    _isPause=YES;
+    [_startBtn setTitle:@"继续播放" forState:UIControlStateNormal];
+    _startBtn.backgroundColor=[UIColor colorWithWhite:0.5 alpha:0.5];
+    [self.player pause];
+
+}
+//开始播放
+-(void)startPlay
+{
+    _startBtn.titleLabel.text=@"";
+    [_startBtn setTitle:@"" forState:UIControlStateNormal];
+    _startBtn.backgroundColor=[UIColor clearColor];
+    [self.player play];
+    
+    [self startTimer];
+
+    _isPause=NO;
+
+}
+//结束播放
+-(void)endPlay
+{
+    [_startBtn setTitle:@"重播" forState:UIControlStateNormal];
+    _startBtn.backgroundColor=[UIColor colorWithWhite:0.5 alpha:0.5];
+    [self endTimer];
+}
+#pragma mark - 获取播放的时间
 - (NSTimeInterval) playableDuration
 {
 
@@ -138,9 +227,7 @@
         return CMTimeGetSeconds(self.playerItem.duration);
 
     }
-
     else
-
     {
 
         return(CMTimeGetSeconds(kCMTimeInvalid));
@@ -155,7 +242,7 @@
     
     if (item.status == AVPlayerItemStatusReadyToPlay) {
         
-        NSLog(@"%f\n",CMTimeGetSeconds(self.playerItem.currentTime));
+   //     NSLog(@"%f\n",CMTimeGetSeconds(self.playerItem.currentTime));
         
         if (!_playBeginState&&CMTimeGetSeconds(self.playerItem.currentTime)==CMTimeGetSeconds(self.playerItem.duration)) {
             
@@ -172,7 +259,7 @@
         return(CMTimeGetSeconds(kCMTimeInvalid));
     }
 }
-
+#pragma mark - 加载视频完成
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
 {
     if ([keyPath isEqualToString:@"status"])
@@ -180,42 +267,31 @@
         if (AVPlayerItemStatusReadyToPlay == self.player.currentItem.status)
         {
            // [self.player play];
-            
-            NSLog(@"%lf",[self playableDuration]);
-            [UIView animateWithDuration:[self playableDuration] animations:^{
-                _progressView.frame=CGRectMake(_progressView.frame.origin.x, _progressView.frame.origin.y, 320, _progressView.frame.size.height);
-            
-            }];
+ 
+
+            NSLog(@"准备播放");
+//            NSLog(@"%lf",[self playableDuration]);
+//            [UIView animateWithDuration:[self playableDuration] animations:^{
+//            
+//            }];
 
         }
     }
 }
-
-- (void)playerItemDidReachEnd: (NSNotification *)notification
+#pragma mark - 播放结束时
+- (void)playerItemDidReachEnd:(NSNotification *)notification
 {
     // Loop the video
-   NSLog(@"%@",notification.object);
-    [_startBtn setTitle:@"重播" forState:UIControlStateNormal];
-    _startBtn.backgroundColor=[UIColor colorWithWhite:0.5 alpha:0.5];
 
-        // Get the current item
-//        AVPlayerItem *playerItem = [self.player currentItem];
-//        // Set it back to the beginning
-//        [playerItem seekToTime: kCMTimeZero];
-//        // Tell the player to do nothing when it reaches the end of the video
-//        // -- It will come back to this method when it's done
-//        self.player.actionAtItemEnd = AVPlayerActionAtItemEndNone;
-//        // Play it again, Sam
+    [self endPlay];//播放停止
+    
 //        [self.player play];
 //    }else {
 //        mPlayer.actionAtItemEnd = AVPlayerActionAtItemEndAdvance;
 //    }
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 
 /*
 #pragma mark - Navigation
