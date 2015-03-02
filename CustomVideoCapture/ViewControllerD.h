3b56c28ea018cc0782ecc25ae2a691dce7c81ffe
q858333 <284825870@qq.com>
2015-03-02 11:26:10 +0800

    test

---
+++ CustomVideoCapture/ViewControllerD.h
@@ -0,0 +1,13 @@
+//
+//  ViewControllerD.h
+//  CustomVideoCapture
+//
+//  Created by dengbin on 15/3/2.
+//  Copyright (c) 2015年 IUAIJIA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewControllerD : UIViewController
+
+@end
---
+++ CustomVideoCapture/ViewControllerD.m
@@ -0,0 +1,37 @@
+//
+//  ViewControllerD.m
+//  CustomVideoCapture
+//
+//  Created by dengbin on 15/3/2.
+//  Copyright (c) 2015年 IUAIJIA. All rights reserved.
+//
+
+#import "ViewControllerD.h"
+
+@interface ViewControllerD ()
+
+@end
+
+@implementation ViewControllerD
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
